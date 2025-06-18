cc11256a3c501ab7733299c62f2f257031016161
Hasnat Hossain <hasnat4222@gmail.com>
2025-06-18 15:44:03 +0600

    Add basic input-output program

---
+++ basic/input_output.c
@@ -0,0 +1,13 @@
+// Program: Input and Output of an Integer
+// Author: Hasnat
+// Description: Takes an integer input from user and prints it.
+
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d", &a);
+    printf("Enter an integer: ");
+    printf("You entered:%d\n",a);
+    return 0;
+}
