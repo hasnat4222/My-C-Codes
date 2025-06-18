3cfb808d5a28fb1294fcc79eae454d30665e75cf
Hasnat Hossain <hasnat4222@gmail.com>
2025-06-18 15:38:58 +0600

    Add welcome message program in basic folder

---
+++ basic/welcome_message.c
@@ -0,0 +1,10 @@
+// Program: Welcome Message
+// Author: Hasnat
+// Description: Prints a welcome message to the programming world.
+
+#include <stdio.h>
+int main ()
+{
+    printf("Welcome to the programming world.\n");
+    return 0;
+}
